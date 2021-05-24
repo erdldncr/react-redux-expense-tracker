f6baf7f52f4e11f451ec40f257ed06a3e51deec7
erdldncr <erdldncr@gmail.com>
2021-05-24 12:53:39 +0100

    redux has been set

---
+++ src/reducer/index.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+
+}
+
+function reducer (state = initialState, { type, payload })  {
+    switch (type) {
+
+
+    default:
+        return state
+    }
+}
+export default reducer
